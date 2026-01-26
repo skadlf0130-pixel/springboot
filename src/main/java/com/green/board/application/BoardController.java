ff77f85033aeaca834676b6b35fac33faba393d6
1ethon0822 <1ethyjj9240@gmail.com>
2026-01-26 18:12:07 +0900

    2026.01.26

---
+++ src/main/java/com/green/board/application/BoardController.java
@@ -0,0 +1,23 @@
+package com.green.board.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @PostMapping("/board")
+    public String postBoard(){
+        return "이것은 포스트 통신입니다.";
+    }
+
+    @GetMapping("/board")
+    public String getBoard(@RequestParam int id ,
+                           @RequestParam String title){
+        System.out.println("id: "+ id);
+        System.out.println("title: "+ title);
+        return "뚱인데여~~";
+    }
+}
